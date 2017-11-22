7b21e181dfc66709394c380b1578951f5541e389
T4vxo <daniel.lundberg@utb.vxo.se>
2017-11-22 10:37:15 +0100

    created components
    created a few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StartComponent } from './start/start.component';
+import { MembersComponent } from './members/members.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { BloggComponent } from './blogg/blogg.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StartComponent,
+    MembersComponent,
+    FooterComponent,
+    ContactComponent,
+    SponsorsComponent,
+    BloggComponent
   ],
   imports: [
     BrowserModule
